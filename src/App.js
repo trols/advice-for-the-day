530043fa999b5b61dada43bef9f3f7d6e508cd65
Olga Travina <110350045+trols@users.noreply.github.com>
2022-10-11 00:41:07 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useEffect,useState} from "react";
+
 
 function App() {
+const[advice,setAdvice]=useState(" ");
+const[type,setType]=useState("");
+const [participants, setParticipants] = useState("");
+  useEffect(()=>{
+    getAdvice()},[])
+    const getAdvice=async()=>{
+      const response=await fetch(`http://www.boredapi.com/api/activity/`)
+      const data=await response.json()
+      console.log(data);
+      setAdvice(data.activity)
+      setType(data.type)
+      setParticipants(data.participants);
+    }
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='App'>
+      
+      <div>
+        <h1>What would you like to do today?</h1>
+      </div>
+      <form>
+<button onClick={()=>getAdvice()}>advice for the day</button>
+</form>
+<div className='block'>
+<h2 className='typewriter'>{advice}</h2>
+
+<p>Type of activity:  <span>{type}</span></p>
+<h3>{participants === 1 ? "for " +  participants + " participant" : "for " +  participants  +  " participants"}</h3>
+</div></div>
   );
 }
 
+
+
 export default App;
+
+
+
+
+
+
+
+
